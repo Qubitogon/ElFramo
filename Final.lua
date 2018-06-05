20dfd3907f6af99ae76fa1340c442e9471253615
Daniel Puth <dputh@student.ethz.ch>
2018-06-05 15:12:33 +0200

    Final.lua naming syntax

--- Final.lua
+++ Final.lua
@@ -4,10 +4,10 @@ print("----EF Final.lua init")
 --the do ... end is currently redundant
 do 
 
-  ElFramo.Group_update() 
-  ElFramo.Tracker_update()
-  ElFramo.FirstDraw_Frames()
-  ElFramo.CreateFamilyFrames()
-  --ElFramo.Frames_update_health_of(1) --DONE FOR TESTING PURPOSES, FEEL FREE TO REMOVE WHEN NOT NEEDED
+  elFramo.groupUpdate() 
+  elFramo.trackerUpdate()
+  elFramo.firstDrawFrames()
+  elFramo.createFamilyFrames()
+  --elFramo.framesUpdateHealthOf(1) --DONE FOR TESTING PURPOSES, FEEL FREE TO REMOVE WHEN NOT NEEDED
 
 end
